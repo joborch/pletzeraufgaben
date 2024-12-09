235aab8417fca1168e5e96afe667345838fb8a27
Jonas Borchmann <jonas.borchmann@stud.plus.ac.at>
2024-12-09 15:19:35 +0100

    Graph erstellt

--- Aufgabenblatt3_JonasBorchmann.m
+++ Aufgabenblatt3_JonasBorchmann.m
@@ -7,3 +7,18 @@ signal=y+0.2*rauschen-0.1;
 smooth_signal = moving_average(signal);
 
 smooth_signal
+
+
+%Plots Planen
+plot(x, y, 'black*'); %Sinus Plot erstellen
+hold on; %Hold an damit Graph ergänzt wird
+plot(x, signal, 'r-'); %Signal Graph ergänzen
+plot(x, smooth_signal, 'b-'); %Gleitenden Mittelwert Graph ergänzen
+
+%Legende schreiben
+title('Noisy Sine Wave'); %Graph einen Titel geben
+legend('Original Sine Wave', 'Sine with Random Noise', 'Smoothed Sine Wave','Location', 'south', 'Orientation', 'horizontal'); %Legende programmieren
+grid on; %Gitternetz anschalten
+
+hold off; %Graph anzeigen
+
